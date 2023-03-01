c22e360014e32f535a33ec0035de53de07fd382c
Shorena K. Anzhilov <100386342+ShorenaK@users.noreply.github.com>
2023-03-01 18:56:03 -0500

    Time: 59 ms (97.24%), Space: 43.2 MB (23.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,21 +5,37 @@
  */
 
 
+// var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i ++){
+//         for (let j = i +1 ; j < nums.length; j ++){
+//             sum = nums[i] + nums[j]
+//             if (sum === target){
+//                 return [i, j]
+//             }
+//         }
+//     }
+// };
 
 
-let twoSum = function (nums, target){
-    const hash = {};
-for(let i = 0; i < nums.length; i++ ){
-    let val =  nums[i];
-    hash[val] = i;
 
-}
-for (let i = 0; i < nums.length; i ++){
-    let potentialKey = target - nums[i]
-    if(hash[potentialKey] && hash[potentialKey] !== i ){
-        return [i, hash[potentialKey]]
+function twoSum(nums, target) {
+  // Create an empty object to store the numbers in the array and their indices
+  const hash = {};
+
+  // Iterate through the array
+  for (let i = 0; i < nums.length; i++) {
+    // Calculate the complement of the current number
+    const complement = target - nums[i];
+
+    // If the complement is in the hash table, return the indices of the two numbers
+    if (hash[complement] !== undefined) {
+      return [hash[complement], i];
     }
 
-}
+    // Add the current number to the hash table with its index as the value
+    hash[nums[i]] = i;
+  }
 
+  // If no pair of numbers adds up to the target, return an empty array
+  return [];
 }
