66c5e8a563bbdcaac1bf5c51d75e4d59fccd2607
Shorena K. Anzhilov <100386342+ShorenaK@users.noreply.github.com>
2023-03-02 18:19:29 -0500

    Time: 187 ms (47.57%), Space: 51 MB (55.75%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,7 +2,20 @@
  * @param {number} x
  * @return {boolean}
  */
+// var isPalindrome = function(x) {
+//     let reversed = x.toString().split('').reverse().join('')
+//     return (x.toString() === reversed)
+// };
+
 var isPalindrome = function(x) {
-    let reversed = x.toString().split('').reverse().join('')
-    return (x.toString() === reversed)
+    if (x < 0) {
+        return false;
+    }
+    let reverse = 0;
+    let num = x;
+    while (num > 0) {
+        reverse = (reverse * 10) + (num % 10);
+        num = Math.floor(num / 10);
+    }
+    return reverse === x;
 };
